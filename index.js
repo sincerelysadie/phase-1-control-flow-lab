c650400302ab42880a439340a4c05309812406b3
Sadie <sadie@Sadies-MacBook-Pro.local>
2022-03-29 13:27:55 -0700

    commit tree 0bf530731d980cc04a09f01b17bb79901101d524

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  console.log(ride);
+  let distance
+  if (ride <= 400) {
+    return 'This one is on me!';
+  }  
+    if (ride > 2000 && ride < 2500) {
+      return 'I will gladly take your thirty bucks.';
+    }
+      if (ride >= 2501) {
+        return 'No can do.'; 
+      }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const destination = (city === NYC) ? true : false;
+  return;
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+
 }
